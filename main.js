2ec24944aa87a06075ec7de8f2b3f347e519a218
Sunclearlove <19855653430@163.com>
2020-02-12 13:32:28 +0800

    request

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App'
+import https from './utils/https.js'
 
 Vue.config.productionTip = false
-
+Vue.prototype.request = https
 App.mpType = 'app'
 
 const app = new Vue({
--- pages/tabBar/home/home.vue
+++ pages/tabBar/home/home.vue
@@ -1,7 +1,40 @@
 <template>
+	<view>
+		<pageHeader />
+	</view>
 </template>
 
 <script>
+	import pageHeader from "./pageHeader.vue"
+	import interfaces from "../../../utils/interfaces.js"
+	export default {
+		data(){
+			return{
+				swiperList:[], 	 //轮播图
+				categoryList:[], //分类
+				promotion:[]     //活动
+			}
+		},
+		components:{
+			pageHeader
+		},
+		methods:{
+			initData(){
+				this.request({
+				    url: interfaces.getMallData, //仅为示例，并非真实接口地址。
+				    success: ((res) =>{
+						    console.log(res.data);
+							this.swiperList = res.data.swiperList;
+							this.categoryList = res.data.categoryList;
+							this.promotion = res.data.promotion;
+					})
+				});
+			}
+		},
+		onLoad() {
+			this.initData();
+		}
+	}
 </script>
 
 <style>
